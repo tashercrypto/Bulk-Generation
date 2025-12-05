cd1ffaf1f9511d70df9eac78672e4678ccf9706f
tashercrypto <154053732+tashercrypto@users.noreply.github.com>
2025-12-05 20:01:24 +0200

    Update API.js

--- js/API.js
+++ js/API.js
@@ -1,29 +1,29 @@
-// API.js
+
 async function loadLocalLogo() {
-  const response = await fetch("https://tashercrypto.github.io/Bulk-Generation/img/logo.jpg");
+  const response = await fetch("img/logo.jpg");
   const blob = await response.blob();
-  return new File([blob], "logo.jpg", { type: "image/jpg" });
+  return new File([blob], "logo.jpg", { type: "image/jpeg" });
 }
+
 
 export async function editUserImage(userImageFile, prompt) {
   const formData = new FormData();
   const logoFile = await loadLocalLogo();
 
-  formData.append("images", userImageFile);   
+  formData.append("images", userImageFile);
+  formData.append("images", logoFile);
   formData.append("prompt", prompt);
-  formData.append("logo", logoFile);
 
-  const res = await fetch("https://bulk-generation-backend.onrender.com/generate-image", {
-    method: "POST",
-    body: formData,
-  });
+const response = await fetch("https://bulk-generation-backend.onrender.com/edit-image", {
+  method: "POST",
+  mode: "cors",
+  body: formData
+});
 
-  if (!res.ok) {
-    const text = await res.text();
-    throw new Error("Server error: " + text.slice(0, 200));
-  }
 
-  const data = await res.json();
+  const data = await response.json();
+  if (data.error) throw new Error(data.error.message);
 
-  return data.url; // backend повертає url
+  return "data:image/png;base64," + data.data[0].b64_json;
 }
+

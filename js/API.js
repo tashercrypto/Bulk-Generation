be7b2e37cdf575e886755cf22dc9728fe9126c84
tashercrypto <154053732+tashercrypto@users.noreply.github.com>
2025-12-05 16:47:03 +0200

    Update API.js

--- js/API.js
+++ js/API.js
@@ -10,15 +10,22 @@ export async function editUserImage(userImageFile, prompt) {
   const formData = new FormData();
   const logoFile = await loadLocalLogo();
 
-  formData.append("images", userImageFile);
-  formData.append("images", logoFile);
+  formData.append("image[]", userImageFile);
+  formData.append("image[]", logoFile);
   formData.append("prompt", prompt);
 
-const response = await fetch("https://bulk-generation-backend.onrender.com/edit-image", {
-  method: "POST",
-  mode: "cors",
-  body: formData
-});
+  const response = await fetch("https://bulk-generation-backend.onrender.com/edit-image", {
+    method: "POST",
+    mode: "cors",
+    body: formData
+  });
+
+  const data = await response.json();
+  if (data.error) throw new Error(data.error.message);
+
+  return "data:image/png;base64," + data.data[0].b64_json;
+}
+
 
 
   const data = await response.json();

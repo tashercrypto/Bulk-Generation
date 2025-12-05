3446789d3b77c30eb25556b2ff2f562af3bb3257
tashercrypto <154053732+tashercrypto@users.noreply.github.com>
2025-12-05 18:34:03 +0200

    Update API.js

--- js/API.js
+++ js/API.js
@@ -1,7 +1,7 @@
 async function loadLocalLogo() {
-  const response = await fetch("https://tashercrypto.github.io/Bulk-Generation/img/logo.jpg");
+  const response = await fetch("img/logo.jpg");
   const blob = await response.blob();
-  return new File([blob], "logo.png", { type: "image/png" });
+  return new File([blob], "logo.jpg", { type: "image/jpg" });
 }
 
 
